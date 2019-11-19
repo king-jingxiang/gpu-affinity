a266849e14de86e4d70cec5f3f58f8e985ce14b7
JingXiang <jingxiang@leinao.ai>
2019-11-19 15:58:12 +0800

    update

--- pkg/infiniband/ibverbs_test.go
+++ pkg/infiniband/ibverbs_test.go
@@ -1,2 +1,15 @@
-package infiniband
+package main
 
+import (
+	"fmt"
+	"github.com/ruanxingbaozi/gpu-affinity/pkg/infiniband/ibverbs"
+)
+
+func main() {
+
+	ibvDevList, err := ibverbs.IbvGetDeviceList()
+	if err != nil {
+		fmt.Println("error")
+	}
+	fmt.Println(ibvDevList)
+}
